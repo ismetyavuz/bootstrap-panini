f29470627b80cdf394e7e100f22853e976c835fa
Ismet Yavuz <info@ismetyavuz.com>
2020-03-31 21:22:42 +0200

    initial commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import 'what-input';
+
+window.jQuery = $;
+require('@popperjs/core');
+require('bootstrap/dist/js/bootstrap.bundle');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
